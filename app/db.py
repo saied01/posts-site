047d144ffd8589e943bd78f1cd46415a92f846c8
saied <rama.saied11@gmail.com>
2026-01-10 12:08:35 -0300

    *

--- app/db.py
+++ app/db.py
@@ -17,7 +17,7 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    caption = Column(Text)
+    caption = Column(Text, nullable=False)
     url = Column(String, nullable=True)
     file_type = Column(String, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
